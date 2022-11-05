0ff08e6044b8101db593a0eeedae85406b0368de
Nalini Mehendale <nalini.mehendale@sjsu.edu>
2022-11-04 23:15:18 -0700

    add schedule frontend with backend api

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom";
+import React, { useState } from "react";
+
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home'
+import Addschedule from './components/Addschedule/addschedule';
 
 
 function App() {
+  
+
   return (
     <BrowserRouter>
       <div class="App">
@@ -15,6 +20,7 @@ function App() {
         <div>
           <Routes>
             <Route path='/' element={<Home/>} />
+            <Route path="addschedule" element={<Addschedule />} />
           </Routes>
         </div>
         <Footer/>
--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.get("/api/selectAllFromRoles",(req,res)=>{
 });
 
 app.get("/",(req,res)=>{
-    const query="Select * fr om roles;"
+    const query="Select * from roles;"
     db.query(query,(error,result)=>{
         if(error==null){
             res.send(JSON.stringify(result));
@@ -40,6 +40,32 @@ app.get("/",(req,res)=>{
         }
     });
 })
+
+app.post("/addschedule", (req,res) => {
+    //console.log(req.body.airlineName);
+    var airlineName = req.body.airlineName;
+    var airportCode = req.body.airportCode;
+    var arrivaltime = req.body.arrivaltime;
+    var gate_num = req.body.gate_num;
+    var terminal_id = req.body.terminal_id;
+    var flight_number_fk = req.body.flight_number_fk;
+    var source = req.body.source;
+    var destination = req.body.destination;
+
+    console.log(airlineName + airportCode + arrivaltime + gate_num + terminal_id + flight_number_fk + source + destination);
+
+    const query="INSERT INTO schedules (airlineName, airportCode, arrivaltime, gate_num, terminal_id, flight_number_fk, source , destination) Values (?,?,?,?,?,?,?,?)";
+    db.query(query,[airlineName, airportCode, arrivaltime, gate_num, terminal_id, flight_number_fk, source , destination],(error,result)=>{
+        if(error==null){
+            //res.send(JSON.stringify(result));
+            res.send("db record added");
+        }
+        else{
+            res.send("An error has occured");
+            console.log(error)
+        }
+    });
+});
 
 app.listen(5000, () => {
     console.log("Its running")
